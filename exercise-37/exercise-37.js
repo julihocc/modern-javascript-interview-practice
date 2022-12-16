972fb6966b3605357a70aa357846b943de5e1690
Juliho Castillo <julihocc@users.noreply.github.com>
2022-12-16 11:01:05 -0600

    Exercises, done

---
+++ exercise-37/exercise-37.js
@@ -0,0 +1,15 @@
+String.prototype.exclamation = function (){
+    // console.log(this);
+    return this + '!';
+}
+
+console.log('hello'.exclamation());
+console.log('hello world'.exclamation());
+
+const add =(...args) => {
+    return args.reduce((a,b) => a + b);
+}
+
+console.log(add(2,5))
+console.log(add(7,11))
+console.log(add(12, 8, 5, 6))
