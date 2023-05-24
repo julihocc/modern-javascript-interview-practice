cbbfa5c291f15dd73d7bec0665258947453b0654
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-24 11:54:11 -0600

    exercise 8 updated

--- es6-solutions/exercise-08/exercise-08.js
+++ es6-solutions/exercise-08/exercise-08.js
@@ -1,31 +1,20 @@
+const testArray = [21, 0, 4, 5, 0, 22, 0, 0, 3]
 
-const testArray = [21,0,4,5,0,22,0,0,3]
-
-function swap(arr, xp, yp)
-{
-    const temp = arr[xp];
-    arr[xp] = arr[yp];
-    arr[yp] = temp;
-}
-
-// https://www.geeksforgeeks.org/bubble-sort/
-// An optimized version of Bubble Sort
-function bubbleSortModified( arr) {
-    const n = arr.length;
-    let i, j;
-    for (i = 0; i < n-1; i++)
-    {
-        for (j = 0; j < n-i-1; j++)
-        {
-            if (arr[j] > arr[j+1] && arr[j+1] === 0)
-            {
-                swap(arr,j,j+1);
-
-            }
+function solution(arr) {
+    // insertamos los elementos no nulos al inicio del array
+    let insertPos = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== 0) {
+            arr[insertPos] = arr[i];
+            insertPos += 1;
         }
-
+    }
+    // rellenamos los lugares restantes con ceros
+    while (insertPos < arr.length) {
+        arr[insertPos] = 0;
+        insertPos += 1;
     }
 }
 
-bubbleSortModified(testArray)
+solution(testArray)
 console.log(testArray)
