ab82ea71499e2448fa5cb88d52f11f3989d1fa2f
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-24 17:23:46 -0600

    Update app.js

--- es6-solutions/exercise-17/app.js
+++ es6-solutions/exercise-17/app.js
@@ -1,20 +1,21 @@
-var candidate = {
+const candidate = {
     name: {
         firstname: 'John',
         lastname: 'Galt',
         phone: '123-456-7890'
+    },
+    printName: function () {
+        return this.name.firstname + ' ' + this.name.lastname;
     }
-}
+};
 
-candidate.printName = function (){
-    const fullName = `${this.name.firstname} ${this.name.lastname}`
-    return fullName
-}
+// Select the second div with class "info"
+const secondInfoDiv = document.querySelector('#body > .info');
 
-const info = document.getElementsByClassName("info")
+// Insert the candidate's first name into the first child of the second info div
+const firstChildDiv = secondInfoDiv.querySelector('div:first-child');
+firstChildDiv.textContent = candidate.name.firstname;
 
-// info[1].children[0].textContent = candidate.name.firstname
-// info[1].children[1].textContent = candidate.printName()
-
-const candidateInfo = info[info.length - 1].firstChild
-candidateInfo.textContent = candidate.printName()
+// Insert the full name into the second div using the printName method
+const secondChildDiv = secondInfoDiv.querySelector('div:nth-child(2)');
+secondChildDiv.textContent = candidate.printName();
