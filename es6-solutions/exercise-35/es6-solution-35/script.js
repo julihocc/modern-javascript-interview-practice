bd18fe899e1b08f058cf9102cd4f85d0f25cd84e
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-25 16:42:11 -0600

    Update script.js

--- es6-solutions/exercise-35/es6-solution-35/script.js
+++ es6-solutions/exercise-35/es6-solution-35/script.js
@@ -1,46 +1,33 @@
-
-
 const table = document.querySelector('table');
-const btn = document.getElementById('btn')
-const rows = table.getElementsByTagName('tr')
-
+const btn = document.getElementById('btn');
+const rows = Array.from(table.getElementsByTagName('tr'));
 
 const swap = (xp, yp) => {
-    // console.log('swapping')
     const temp = rows[xp].innerHTML;
-    // console.log('temp',temp)
     rows[xp].innerHTML = rows[yp].innerHTML;
     rows[yp].innerHTML = temp;
-    // console.log('arr',rows)
-}
+};
 
 const getAge = (row) => {
     const age = row.getElementsByTagName('td')[1];
-    return age.textContent;
-}
-function bubbleSort( )
-{
-    // console.log(Array.from(rows).map(getAge));
+    return parseInt(age.textContent);
+};
+
+function bubbleSort() {
     const n = rows.length;
     let i, j;
-    for (i = 1; i < n; i++)
-    {
-        for (j = 1; j < n-i; j++)
-        {
-            // console.log(i,j)
+    for (i = 1; i < n; i++) {
+        for (j = 1; j < n - i; j++) {
             const age1 = getAge(rows[j]);
-            const age2 = getAge(rows[j+1]);
-            // console.log(age1,age2)
-            if ( age1 > age2)
-            {
-                swap(j,j+1);
+            const age2 = getAge(rows[j + 1]);
+            if (age1 > age2) {
+                swap(j, j + 1);
             }
         }
-
     }
 }
 
-btn.addEventListener('click', event=>{
-    event.preventDefault()
-    bubbleSort()
-})
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    bubbleSort();
+});
