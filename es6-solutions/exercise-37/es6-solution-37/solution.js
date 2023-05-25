7f35ec2f893310b38c200ebaab9062b0435f809b
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-25 16:46:58 -0600

    Update solution.js

--- es6-solutions/exercise-37/es6-solution-37/solution.js
+++ es6-solutions/exercise-37/es6-solution-37/solution.js
@@ -1,4 +1,4 @@
-String.prototype.exclamation = function (){
+String.prototype.exclamation = function () {
     // console.log(this);
     return `${this}!`;
 }
@@ -6,17 +6,12 @@ String.prototype.exclamation = function (){
 console.log('hello'.exclamation());
 console.log('hello world'.exclamation());
 
-const add =(...args) => {
-    if (args.length>0){
-        return args.reduce((a,b) => a + b);
-    } else {
-        console.error('Not enough arguments');
-        return null
-    }
+const add = (...args) => {
+    return args.reduce((a, b) => a + b, 0);
 }
 
-console.log(add(2,5))
-console.log(add(7,11))
+console.log(add(2, 5))
+console.log(add(7, 11))
 console.log(add(12, 8, 5, 6))
 console.log(add(1))
 console.log(add())
