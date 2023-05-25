d8b64092d830e3cabff85de37a9359d6fe425599
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-25 12:52:21 -0600

    solution 30

--- es6-solutions/exercise-30/es6-solution-30/script.js
+++ es6-solutions/exercise-30/es6-solution-30/script.js
@@ -1,20 +1,38 @@
+//
+// const p = document.querySelectorAll('p')
+//
+// for(const idx in p){
+//     if(idx%2===1){
+//         p[idx].style.display = 'none'
+//     }
+//     else{
+//         p[idx].addEventListener('click', function(){
+//             let nextIdx = parseInt(idx)+1
+//             let nextP = p[nextIdx]
+//             let display = nextP.style.display
+//             if(display==='none'){
+//                 nextP.style.display = 'block'
+//             } else {
+//                 nextP.style.display = 'none'
+//             }
+//         });
+//     }
+// }
 
-const p = document.querySelectorAll('p')
+document.addEventListener('DOMContentLoaded', function() {
+    const pElements = document.querySelectorAll('p');
 
-for(const idx in p){
-    if(idx%2===1){
-        p[idx].style.display = 'none'
+    for (let i = 1; i < pElements.length; i += 2) {
+        pElements[i].style.display = 'none';
     }
-    else{
-        p[idx].addEventListener('click', function(){
-            let nextIdx = parseInt(idx)+1
-            let nextP = p[nextIdx]
-            let display = nextP.style.display
-            if(display==='none'){
-                nextP.style.display = 'block'
-            } else {
-                nextP.style.display = 'none'
+
+    document.addEventListener('click', function(event) {
+        const target = event.target;
+        if (target.tagName === 'P') {
+            const nextP = target.nextElementSibling;
+            if (nextP && nextP.tagName === 'P') {
+                nextP.style.display = (nextP.style.display === 'none') ? 'block' : 'none';
             }
-        });
-    }
-}
+        }
+    });
+});
