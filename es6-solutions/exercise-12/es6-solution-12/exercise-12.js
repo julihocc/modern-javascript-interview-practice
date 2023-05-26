99a6adb1f51b0041fffd5100d9b27757a46923a0
Juliho Castillo <julihocc@users.noreply.github.com>
2023-05-26 10:16:58 -0600

    Update exercise-12.js

--- es6-solutions/exercise-12/es6-solution-12/exercise-12.js
+++ es6-solutions/exercise-12/es6-solution-12/exercise-12.js
@@ -1,20 +1,46 @@
+function adjustTimestamp(initialTimestamp, shiftMagnitude, shiftUnit) {
+    let givenTimestamp = new Date(initialTimestamp);
+    let adjusterValue;
 
+    if (shiftUnit == 'day' || shiftUnit == 'days') {
+        adjusterValue = shiftMagnitude * 24 * 60 * 60 * 1000; // days to milliseconds
+        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+    }
+    else if (shiftUnit == 'week' || shiftUnit == 'weeks') {
+        adjusterValue = shiftMagnitude * 7 * 24 * 60 * 60 * 1000; // weeks to milliseconds
+        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+    }
+    else if (shiftUnit == 'month' || shiftUnit == 'months') {
+        givenTimestamp.setMonth(givenTimestamp.getMonth() + shiftMagnitude);
+    }
+    else if (shiftUnit == 'hour' || shiftUnit == 'hours') {
+        adjusterValue = shiftMagnitude * 60 * 60 * 1000; // hours to milliseconds
+        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+    }
+    else if (shiftUnit == 'minute' || shiftUnit == 'minutes') {
+        adjusterValue = shiftMagnitude * 60 * 1000; // minutes to milliseconds
+        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+    }
+    else if (shiftUnit == 'second' || shiftUnit == 'seconds') {
+        adjusterValue = shiftMagnitude * 1000; // seconds to milliseconds
+        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+    }
+    else {
+        console.log("Sorry, I didn't get that. Could you try with 'day', 'week', 'month', 'hour', 'minute', or 'second'?");
+        return;
+    }
 
-class DateTime extends Date {
-    constructor() {
-        super()
-    }
-    get prop() {
-        return this._prop
-    }
-    set prop(value) {
-        this._prop = value
-    }
+    return givenTimestamp;
 }
 
-console.log(DateTime)
-const dateTime = new DateTime()
-dateTime.prop = 0
-console.log(dateTime)
-dateTime.prop = 1
-console.log(dateTime)
+let currentTimestamp = new Date();
+console.log("Now it's: " + currentTimestamp);
+
+let shiftedTimestamp = adjustTimestamp(currentTimestamp, -1, 'day');
+console.log("24 hours earlier it was: " + shiftedTimestamp);
+
+shiftedTimestamp = adjustTimestamp(currentTimestamp, 2, 'months');
+console.log("Two months from now it'll be: " + shiftedTimestamp);
+
+shiftedTimestamp = adjustTimestamp(currentTimestamp, 45, 'minutes');
+console.log("45 minutes later it'll be: " + shiftedTimestamp);
