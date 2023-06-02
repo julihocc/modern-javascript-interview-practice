7f927dc573a8066d3c94c80697d2a308a3994e21
Juliho Castillo <julihocc@users.noreply.github.com>
2023-06-02 13:24:43 -0600

    Move to the upper level

--- solutions/exercise-24/exercise-24 - Copy.js
+++
@@ -1,7 +0,0 @@
-const fromRemove = (arr,...args) => {
-    return arr.filter(item => !args.includes(item))
-}
-
-const arr = [1,2,3,4,1,2,3,4,5,6]
-const result = fromRemove(arr, 2,4,6)
-console.log(result)
--- solutions/exercise-29/es6-exercise-29-solution/script.js
+++
@@ -1,8 +0,0 @@
-const a = document.querySelector('#id1')
-
-a.addEventListener('click', event => {
-    event.preventDefault()
-    const div = a.previousElementSibling
-    console.log(div)
-    div.style.display = 'block'
-})
