bae5fdf7b0c5bdccddc2d36da825dfda2adc9a02
Juliho Castillo <julihocc@users.noreply.github.com>
2023-06-02 11:12:14 -0600

    it would have been better to take a Date object as an argument rather than a timestamp

--- solutions/exercise-12/solution/main.js
+++ solutions/exercise-12/solution/main.js
@@ -1,36 +1,36 @@
-function adjustTimestamp(initialTimestamp, shiftMagnitude, shiftUnit) {
-    let givenTimestamp = new Date(initialTimestamp);
+function adjustTimestamp(initialDate, shiftMagnitude, shiftUnit) {
+    let modifiedDate = initialDate;
     let adjusterValue;
 
     if (shiftUnit == 'day' || shiftUnit == 'days') {
         adjusterValue = shiftMagnitude * 24 * 60 * 60 * 1000; // days to milliseconds
-        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+        modifiedDate.setTime(modifiedDate.getTime() + adjusterValue);
     }
     else if (shiftUnit == 'week' || shiftUnit == 'weeks') {
         adjusterValue = shiftMagnitude * 7 * 24 * 60 * 60 * 1000; // weeks to milliseconds
-        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+        modifiedDate.setTime(modifiedDate.getTime() + adjusterValue);
     }
     else if (shiftUnit == 'month' || shiftUnit == 'months') {
-        givenTimestamp.setMonth(givenTimestamp.getMonth() + shiftMagnitude);
+        modifiedDate.setMonth(modifiedDate.getMonth() + shiftMagnitude);
     }
     else if (shiftUnit == 'hour' || shiftUnit == 'hours') {
         adjusterValue = shiftMagnitude * 60 * 60 * 1000; // hours to milliseconds
-        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+        modifiedDate.setTime(modifiedDate.getTime() + adjusterValue);
     }
     else if (shiftUnit == 'minute' || shiftUnit == 'minutes') {
         adjusterValue = shiftMagnitude * 60 * 1000; // minutes to milliseconds
-        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+        modifiedDate.setTime(modifiedDate.getTime() + adjusterValue);
     }
     else if (shiftUnit == 'second' || shiftUnit == 'seconds') {
         adjusterValue = shiftMagnitude * 1000; // seconds to milliseconds
-        givenTimestamp.setTime(givenTimestamp.getTime() + adjusterValue);
+        modifiedDate.setTime(modifiedDate.getTime() + adjusterValue);
     }
     else {
         console.log("Sorry, I didn't get that. Could you try with 'day', 'week', 'month', 'hour', 'minute', or 'second'?");
         return;
     }
 
-    return givenTimestamp;
+    return modifiedDate;
 }
 
 let currentTimestamp = new Date();
