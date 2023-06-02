66889d99803b7e01cddf0e98f53177429162bf6d
Juliho Castillo <julihocc@users.noreply.github.com>
2023-06-02 11:03:50 -0600

    - using sets for prop1 and prop2 properties seems unnecessary. The property keys will be unique for each individual object

--- es6-solutions/exercise-11/solution.js
+++ es6-solutions/exercise-11/solution.js
@@ -5,12 +5,18 @@ const difference = (obj1, obj2) => {
     let difference = new Set()
     for (let prop in obj1) {
         if (obj2[prop] === undefined || obj1[prop] !== obj2[prop]) {
-            difference.add(prop)
+            const obj = {
+                [prop]: [obj1[prop], obj2[prop]]
+            }
+            difference.add(JSON.stringify(obj))
         }
     }
     for (let prop in obj2) {
         if (obj1[prop] === undefined || obj1[prop] !== obj2[prop]) {
-            difference.add(prop)
+            const obj = {
+                [prop]: [obj1[prop], obj2[prop]]
+            }
+            difference.add(JSON.stringify(obj))
         }
     }
     return difference
